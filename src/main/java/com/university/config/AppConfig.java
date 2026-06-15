d486a106a83e066cb0bf5ed1ef5acd3a3fdd1b26
kevine umukundwa <kevineumk250@gmail.com>
2026-06-15 20:25:42 +0200

    AppCinfig class

---
+++ src/main/java/com/university/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.university.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.university")
+public class AppConfig {
+}
